9f3a59693631b2ade6d8115166e93dddf0d7199d
StroganovM <johny.black.1991@gmail.com>
2020-05-15 14:31:48 +0300

    Изменил ввод на ввод из произвольного потока

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 vector<double>
-input_numbers(size_t count)
+input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -27,68 +27,28 @@ vector<size_t> make_histogram(vector<double> numbers, size_t bin_count, double& 
     }
     return bins;
 }
-void  show_histogram_text(vector<size_t> bins)
-{
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-    size_t max_count = 0;
-    for (size_t count : bins)
-    {
-        if (count > max_count)
-        {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins)
-    {
-        if (bin < 100)
-        {
-            cout << ' ';
-        }
-        if (bin < 10)
-        {
-            cout << '  ';
-        }
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed)
-        {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-
-        for (size_t i = 0; i < height; i++)
-        {
-            cout << '*';
-        }
-        cout << '\n';
-    }
-}
 int
 main()
 {
-    // Р’РІРѕРґ РґР°РЅРЅС‹С…
+    // Ввод данных
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(in, number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
 
-    // РћР±СЂР°Р±РѕС‚РєР° РґР°РЅРЅС‹С…
+    // Обработка данных
     double min = numbers[0];
     double max = numbers[0];
     find_minmax(numbers, min, max);
 
     vector<size_t> bins = make_histogram(numbers, bin_count, min, max);
-    // Р’С‹РІРѕРґ РґР°РЅРЅС‹С…
+    // Вывод данных
 
 
     show_histogram_svg(bins);
