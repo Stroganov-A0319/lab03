27a327d81b1f44a83581d797215a6a3020756c90
StroganovM <johny.black.1991@gmail.com>
2020-05-31 19:35:42 +0300

    Добавил новый параметр в функцию read_input() новый параметр, котоырй определяет необходимость выводить подсказки

--- main.cpp
+++ main.cpp
@@ -15,18 +15,25 @@ input_numbers(istream& in, size_t count)
     return result;
 }
 Input
-read_input(istream& in) {
+read_input(istream& in, bool prompt) {
     Input data;
-
+    if (prompt)
+    {
     cerr << "Enter number count: ";
+    }
     size_t number_count;
     in >> number_count;
-
+    if (prompt)
+    {
     cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
 
     size_t bin_count;
+    if (prompt)
+    {
     cerr << "Enter column count: ";
+    }
     in >> data.bin_count;
 
     return data;
@@ -35,7 +42,7 @@ read_input(istream& in) {
 int
 main()
 {
-    Input input = read_input(cin);
+    Input input = read_input(cin, true);
     vector<size_t> bins = make_histogram(input);
     show_histogram_svg(bins);
     return 0;
